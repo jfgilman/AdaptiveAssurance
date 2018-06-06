c6b7d0d86a737f007e212ca7ff649875442407fc
jfgilman <jfgilman@ncsu.edu>
2018-06-06 10:38:38 -0400

    added total time version to data simulation function

--- genMPLP.R
+++ genMPLP.R
@@ -2,13 +2,33 @@
 
 
 # for generating modulated power law failure times
-genMPLP <- function(numFail, eta, phi, kappa){
-  times <- rep(0, numFail)
-  times[1] <- eta*rgamma(1, kappa, 1)^(1/phi)
-  for(i in 2:numFail){
-    times[i] <- eta*((times[i-1]/eta)^phi + rgamma(1, kappa, 1))^(1/phi)
+genMPLP <- function(eta, phi, kappa, numFail = NULL, totTime = NULL){
+
+  if(!is.null(numFail)){
+    times <- rep(0, numFail)
+    times[1] <- eta*rgamma(1, kappa, 1)^(1/phi)
+    for(i in 2:numFail){
+      times[i] <- eta*((times[i-1]/eta)^phi + rgamma(1, kappa, 1))^(1/phi)
+    }
+    return(times)
+  }else if(!is.null(totTime)){
+    times <- c()
+    times[1] <- eta*rgamma(1, kappa, 1)^(1/phi)
+    if(times[1] > totTime){
+      print("No Failures")
+      return(NULL)
+    }else{
+      while(tail(times, n=1) < totTime){
+        times <- append(times, eta*((tail(times, n=1)/eta)^phi + rgamma(1, kappa, 1))^(1/phi))
+      }
+      return(head(times, -1))
+    }
+  }else{
+    print("No number of failures or total time included")
+    return(NULL)
   }
-  return(times)
 }
 
-# genMPLP(10, 2, .5, .5)
+genMPLP(2, .5, .5, 10)
+
+genMPLP(2, .5, .5,totTime=30)
